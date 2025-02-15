807df99afa033140b90f84fa20b20b1375cf236d
Gabriel Sanches <gabrielsanches131202@gmail.com>
2025-02-15 14:39:59 -0300

    first commit | Project created

---
+++ src/main/java/com/sanches/financial_management_project/FinancialManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sanches.financial_management_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialManagementProjectApplication.class, args);
+	}
+
+}
