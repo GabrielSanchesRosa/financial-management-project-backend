212c85e0150b500c4eda96d826403ad452aa1113
Gabriel Sanches <gabrielsanches131202@gmail.com>
2025-03-03 18:09:03 -0300

    feat: Adicionando configuração de segurança de endpoints

---
+++ src/main/java/com/sanches/financial_management_project/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.sanches.financial_management_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Desativa CSRF (opcional, útil para APIs REST)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/users/**").permitAll() // Permite acesso público
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/users/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/users/**").authenticated()
+                        .anyRequest().authenticated() // Exige autenticação para o restante
+                )
+                .httpBasic(httpBasic -> {}) // Garante que Basic Auth está ativado
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));;
+
+        return http.build();
+    }
+}
