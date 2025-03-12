e5acf7e4e4fc914f7218fc9e5eca9092b2af2eec
Gabriel Sanches <gabrielsanches131202@gmail.com>
2025-03-11 22:19:06 -0300

    feat: implementando funcionamento de usuários com spingSecurity

--- src/main/java/com/sanches/financial_management_project/config/SecurityConfig.java
+++ src/main/java/com/sanches/financial_management_project/config/SecurityConfig.java
@@ -1,33 +1,54 @@
 package com.sanches.financial_management_project.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) // Desativa CSRF (opcional, útil para APIs REST)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/users/**").permitAll() // Permite acesso público
-                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/users/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/users/**").authenticated()
+//                        .requestMatchers(HttpMethod.GET, "/**").permitAll() // Permite acesso público
+//                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+//                        .requestMatchers(HttpMethod.PUT, "/users/**").authenticated()
+//                        .requestMatchers(HttpMethod.DELETE, "/users/**").authenticated()
                         .anyRequest().authenticated() // Exige autenticação para o restante
                 )
                 .httpBasic(httpBasic -> {}) // Garante que Basic Auth está ativado
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));;
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+
+        return authenticationProvider;
     }
 
     @Bean
---
+++ src/main/java/com/sanches/financial_management_project/service/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.sanches.financial_management_project.service;
+
+import com.sanches.financial_management_project.mapper.UserMapper;
+import com.sanches.financial_management_project.model.UserPrincipal;
+import com.sanches.financial_management_project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private final UserMapper userMapper = UserMapper.INSTANCE;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var entity = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not Found"));
+
+        return new UserPrincipal(userMapper.toDto(entity));
+    }
+}
